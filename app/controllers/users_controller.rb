186d791d1aa16f2e967e740d6723ffee15cfbf08
Topazon <topazonzon@gmail.com>
2019-11-04 09:08:07 +0000

    9.1 department

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -49,7 +49,7 @@ class UsersController < ApplicationController
   private
   
     def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmaiton)
+      params.require(:user).permit(:name, :email, :department, :password, :password_confirmaiton)
     end
     
     
