52d6e38362130a44e4010a9008d93eab670007dd
Topazon <topazonzon@gmail.com>
2019-11-04 09:35:13 +0000

    9.2 basic-info

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,8 @@ class User < ApplicationRecord
                     uniqueness: true
   
   validates :department, length: { in: 2..50 }, allow_blank: true
+  validates :basic_time, presence: true
+  validates :work_time, presence: true
   
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
