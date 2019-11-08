2e0a429a9812435fdf33d7c863da669d8ba6ba79
Topazon <topazonzon@gmail.com>
2019-11-08 16:58:36 +0000

    Finish edit-attendances

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,12 +6,12 @@ class ApplicationController < ActionController::Base
   
     # beforフィルター
 
-  # paramsハッシュからユーザーを取得します。
+  # paramsハッシュからユーザーを取得
   def set_user
     @user = User.find(params[:id])
   end
 
-  # ログイン済みのユーザーか確認します。
+  # ログイン済みのユーザーか確認
   def logged_in_user
     unless logged_in?
       store_location
@@ -20,33 +20,33 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  # アクセスしたユーザーが現在ログインしているユーザーか確認します。
+  # アクセスしたユーザーが現在ログインしているユーザーか確認
   def correct_user
     redirect_to(root_url) unless current_user?(@user)
   end
 
-  # システム管理権限所有かどうか判定します。
+  # システム管理権限所有かどうか判定
   def admin_user
     redirect_to root_url unless current_user.admin?
   end
 
-  # ページ出力前に1ヶ月分のデータの存在を確認・セットします。
+  # ページ出力前に1ヶ月分のデータの存在を確認・セット
   def set_one_month 
     @first_day = params[:date].nil? ? Date.current.beginning_of_month : params[:date].to_date
     @last_day = @first_day.end_of_month
-    one_month = [*@first_day..@last_day] # 対象の月の日数を代入します。
-    # ユーザーに紐付く一ヶ月分のレコードを検索し取得します。
+    one_month = [*@first_day..@last_day] # 対象の月の日数を代入
+    # ユーザーに紐付く一ヶ月分のレコードを検索し取得
     @attendances = @user.attendances.where(worked_on: @first_day..@last_day).order(:worked_on)
 
-    unless one_month.count == @attendances.count # それぞれの件数（日数）が一致するか評価します。
-      ActiveRecord::Base.transaction do # トランザクションを開始します。
-        # 繰り返し処理により、1ヶ月分の勤怠データを生成します。
+    unless one_month.count == @attendances.count # それぞれの件数（日数）が一致するか評価
+      ActiveRecord::Base.transaction do # トランザクションを開始
+        # 繰り返し処理により、1ヶ月分の勤怠データを生成
         one_month.each { |day| @user.attendances.create!(worked_on: day) }
       end
       @attendances = @user.attendances.where(worked_on: @first_day..@last_day).order(:worked_on)
     end
 
-  rescue ActiveRecord::RecordInvalid # トランザクションによるエラーの分岐です。
+  rescue ActiveRecord::RecordInvalid # トランザクションによるエラーの分岐
     flash[:danger] = "ページ情報の取得に失敗しました、再アクセスしてください。"
     redirect_to root_url
   end
