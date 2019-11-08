00086197cfb4d7cbd8d80028183dee1aa15ae061
Topazon <topazonzon@gmail.com>
2019-11-08 13:28:19 +0000

    11.2 StrongParameters

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,4 +1,7 @@
 class AttendancesController < ApplicationController
+  before_action :set_user, only: :edit_one_month
+  before_action :logged_in_user, only: [:update, :edit_one_month]
+  before_action :set_one_month, only: :edit_one_month
   UPDATE_ERROR_MSG = "勤怠登録に失敗しました。やり直してください。"
   
   def update
@@ -19,5 +22,18 @@ class AttendancesController < ApplicationController
       end
     end
     redirect_to @user
+  end
+  
+  
+  def edit_one_month
+  end
+  
+  def update_one_month
+  end
+  
+  private
+  #1か月分の勤怠情報を扱う
+  def attendances_params
+    params.require(:user).permit(attendances: [:started_at, :finished_at, :note])[:attendances]
   end
 end
